eabf3c814445cf05a92e30fe04cd3fe9901e9699
Kristof Dombi <kristof.dombi@gmail.com>
2019-08-14 12:40:37 +0200

    Refactor cryptocurrency.state list to be convert aware

--- src/app/cryptocurrency/cc-list/cc-list.component.ts
+++ src/app/cryptocurrency/cc-list/cc-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
+import { Store } from '@ngxs/store';
 import { SettingsState } from 'app/settings/shared/settings.state';
 import { FetchCryptocurrencyList } from '../shared/cryptocurrency.actions';
 import { FiatCurrency } from 'app/settings/settings/shared/fiat-currency.enum';
 import { CryptocurrencyState } from '../shared/cryptocurrency.state';
 import { Observable } from 'rxjs';
 import { CryptocurrencyObject } from '../shared/models/cryptocurrency-object.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cc-list',
@@ -16,7 +17,7 @@ export class CcListComponent implements OnInit {
 
   static readonly PAGE_SIZE = 10;
 
-  @Select(CryptocurrencyState.cryptocurrencies) cryptocurrencies$: Observable<CryptocurrencyObject[]>;
+  cryptocurrencies$: Observable<CryptocurrencyObject[]>;
 
   currentFiat: FiatCurrency;
 
@@ -24,6 +25,8 @@ export class CcListComponent implements OnInit {
 
   ngOnInit() {
     this.currentFiat = this.store.selectSnapshot(SettingsState.activeFiatCurrency);
+
+    this.initializeCryptocurrencyDetailsSelector();
 
     this.loadList(false);
   }
@@ -34,6 +37,12 @@ export class CcListComponent implements OnInit {
 
   private loadList(force: boolean) {
     this.store.dispatch(new FetchCryptocurrencyList(1, CcListComponent.PAGE_SIZE, this.currentFiat, force));
+  }
+
+  private initializeCryptocurrencyDetailsSelector() {
+    this.cryptocurrencies$ = this.store
+      .select(CryptocurrencyState.cryptocurrencyList)
+      .pipe(map(filterFn => filterFn(this.currentFiat)));
   }
 
 }
--- src/app/cryptocurrency/shared/cryptocurrency.state.ts
+++ src/app/cryptocurrency/shared/cryptocurrency.state.ts
@@ -5,13 +5,16 @@ import { CoinMarketCapService } from './coin-market-cap.service';
 import { tap } from 'rxjs/operators';
 
 export interface CryptocurrencyStateModel {
-  list: CryptocurrencyObject[];
+  /**
+   * Map for `convert` fiat currency
+   */
+  listMap: Map<string, CryptocurrencyObject[]>;
 }
 
 @State<CryptocurrencyStateModel>({
   name: 'cryptocurrency',
   defaults: {
-    list: null
+    listMap: new Map<string, CryptocurrencyObject[]>()
   }
 })
 export class CryptocurrencyState {
@@ -20,24 +23,30 @@ export class CryptocurrencyState {
   }
 
   @Selector()
-  static cryptocurrencies(state: CryptocurrencyStateModel) {
-    return state.list;
+  static cryptocurrencyList(state: CryptocurrencyStateModel) {
+    return (convert: string) => {
+      return state.listMap.get(convert);
+    };
   }
 
   @Action(FetchCryptocurrencyList, { cancelUncompleted: true })
   fetchCryptocurrencyList(context: StateContext<CryptocurrencyStateModel>, action: FetchCryptocurrencyList) {
-    const currentList = context.getState().list;
+    const currentListMap = context.getState().listMap;
 
-    if (!action.force && currentList && currentList.length >= 0) {
+    if (!action.force && currentListMap && currentListMap.has(action.convert)) {
+      console.debug("Found existing list for convert: ", action.convert);
       return;
     }
 
     return this.coinMarketCapService
       .getListingsLatest(action.start, action.limit, action.convert)
       .pipe(tap(res => {
+        const newMap = new Map(currentListMap);
+        newMap.set(action.convert, res);
+
         context.patchState({
-          list: res
-        })
+          listMap: newMap
+        });
       }));
   }
 }
